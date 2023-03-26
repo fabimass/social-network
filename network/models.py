b115b442e35b770fc98c32a4075a07d86f785312
fabimass <massotto.fabian@gmail.com>
2023-03-26 13:40:38 -0300

    adding post model

--- network/models.py
+++ network/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .settings import NEW_POST_LENGTH
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=NEW_POST_LENGTH)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings_owned")
+    liked_by = models.ManyToManyField(User, blank=True, related_name="posts_liked")
+    date_posted = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id}: {self.posted_by} on {self.date_posted}"
