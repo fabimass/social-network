0e609af3d23d330d7474d8203352fa3da98163a4
fabimass <massotto.fabian@gmail.com>
2023-03-26 15:19:35 -0300

    catch the clicks for the likes

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Event listener for when the user is typing a new post
     document.querySelector('#newpost').addEventListener('input', () => calculate_remaining_chars());
+
+    // Event listener for likes
+    document.querySelectorAll('.likes').forEach((element) => element.addEventListener('click', (event) => like(event.target.id)));
     
 });
 
@@ -12,4 +15,8 @@ const calculate_remaining_chars = () => {
     const max_length = parseInt(document.querySelector('#maxlength').value);
     const remaining_chars = max_length - newpost.length;
     span.innerHTML = `${remaining_chars} characters left`;
+}
+
+const like = (id) => {
+    console.log(id);
 }
