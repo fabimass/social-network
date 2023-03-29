5c2a813aa5a0a5f47a086461b704c4213e690c04
fabimass <massotto.fabian@gmail.com>
2023-03-29 08:03:37 -0300

    starting to make user page

---
+++ network/static/network/likes.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelectorAll('.likes').forEach((element) => element.addEventListener('click', (event) => like(event.target)));
+    
+});
+
+
+const like = (target) => {
+
+    fetch(`likes/${target.id}`, {
+        method: 'POST',
+        headers: {
+            "X-CSRFToken": CSRF_TOKEN
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        
+        // Update likes count in the page
+        target.innerHTML = `${(result.status) ? '&#10084;' : '&#9825;'} ${result.count}` 
+    });
+}
